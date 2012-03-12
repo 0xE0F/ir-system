467fabe9f5422f169432d08acfd17f55c0b2be90
Duke <po4ta.duka@gmail.com>
2012-03-12 20:18:57 +0400

    [+] Working mode as leds blink.

--- IRScaner/main.c
+++ IRScaner/main.c
@@ -140,6 +140,8 @@ static void InitLeds(void)
 {
 	GPIO_InitTypeDef  GPIO_InitStructure;
 
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
+
 	GPIO_StructInit(&GPIO_InitStructure);
 	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_8 | GPIO_Pin_9;
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
